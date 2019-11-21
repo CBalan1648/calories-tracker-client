6aae9b5a56f1cddb2f4763427416c3d9ee979061
Catalin Balan <catalin.balan@iquestgroup.com>
2019-11-21 09:27:52 +0200

    Add delete functionality, update data on add/delete

--- src/app/Components/meals/meals.component.ts
+++ src/app/Components/meals/meals.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Meal } from 'src/app/Models/meal';
 import { MealsService } from '../../Services/meals.service';
 
 @Component({
@@ -9,27 +8,17 @@ import { MealsService } from '../../Services/meals.service';
 })
 export class MealsComponent implements OnInit {
 
-  private meals: any;
-  private columsToDisplay: string[] = ['title', 'description', 'time', 'calories'];
-  private mealsObservable ;
+  private columsToDisplay: string[] = ['title', 'description', 'time', 'calories', 'actions'];
+  private mealsObservable;
+  private deleteMeal;
 
-
-  constructor(private readonly mealsService : MealsService) {
-    this.meals = [
-      { id: 1, title: '1st Lunch', description: 'Something somethsomethingsomethingsomethingsomethingsomethingsomethingsomethinging somethsomethingsomething12312312312312312somethingsomethingsomethingsomethingsomethinging somethsomethingsomethingsomethingsomethingsomethingsomethingsomethinging lunch', time: 1574065337366, calories: 650 },
-      { id: 2, title: '1st Dinner', description: 'Something something dinner', time: 1574065237366, calories: 786 },
-      { id: 3, title: '2nd Lunch', description: 'Something something lunch', time: 1574065331366, calories: 540 },
-      { id: 4, title: '2nd Dinner', description: 'Something something dinner', time: 1574065137366, calories: 237 },
-      { id: 5, title: '3rd Lunch', description: 'Something something lunch', time: 1574065137366, calories: 879 },
-      { id: 6, title: '3rd Dinner', description: 'Something something dinner', time: 1574063337366, calories: 865 },
-      { id: 7, title: '4th Lunch', description: 'Something something lunch', time: 1574065334366, calories: 457 },
-    ];
+  constructor(private readonly mealsService: MealsService) {
     this.mealsObservable = this.mealsService.getObservable()
     this.mealsService.getMeals()
+    this.deleteMeal = this.mealsService.deleteMeal.bind(this.mealsService)
   }
-
   ngOnInit() {
-  
+
   }
 
 }
--- src/app/Services/meals.service.ts
+++ src/app/Services/meals.service.ts
@@ -1,7 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable, Subscription } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
-import { retry, take, tap, map } from 'rxjs/operators';
+import { map, retry, take, tap } from 'rxjs/operators';
+import { Meal } from '../Models/meal';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ import { retry, take, tap, map } from 'rxjs/operators';
 export class MealsService {
 
   private currentMealsSubject: BehaviorSubject<any> = new BehaviorSubject<any>(null);
-  public currentMeals: Observable<any> = this.currentMealsSubject.asObservable();
+  public currentMeals: Observable<Meal[]> = this.currentMealsSubject.asObservable().pipe(tap(console.log));
 
   constructor(private http: HttpClient) { }
 
@@ -26,10 +27,13 @@ export class MealsService {
   }
 
   postRequest(mealData) {
-    this.http.post<any>('http://localhost:3000/meals/', mealData).pipe(retry(3), take(1)).subscribe(response => {
-      console.log(response.status)
-      if (response.status === 200) {
-        this.getMeals()
+    this.http.post<any>('http://localhost:3000/meals/', mealData, { observe: 'response' }).pipe(retry(3), take(1)).subscribe(response => {
+      if (response.status === 201) {
+        this.currentMeals.pipe(take(1)).subscribe(currentMealsArray => {
+          const updatedArray = [...currentMealsArray];
+          updatedArray.push(response.body);
+          this.currentMealsSubject.next(updatedArray);
+        });
       }
     });
   }
@@ -38,11 +42,29 @@ export class MealsService {
     return this.currentMeals;
   }
 
-  public getMeals() : void {
-     this.http.get('http://localhost:3000/meals').pipe(
+  deleteMeal(mealId): void {
+    console.log(`DELETE MEAL CALLED WITH ${mealId}`);
+    this.http.delete(`http://localhost:3000/meals/${mealId}`).pipe(
       retry(3),
       take(1),
-      map(responseData =>  responseData.meals)
+    ).subscribe(response => {
+      console.log(response);
+      if (response.nModified === 1) {
+        this.currentMeals.pipe(take(1)).subscribe(currentMealsArray => {
+          const updatedArray = currentMealsArray.filter(meal => meal._id !== mealId);
+          this.currentMealsSubject.next(updatedArray);
+        });
+      }
+
+
+    });
+  }
+
+  public getMeals(): void {
+    this.http.get('http://localhost:3000/meals').pipe(
+      retry(3),
+      take(1),
+      map(responseData => responseData.meals)
     ).subscribe(meals => {
       this.currentMealsSubject.next(meals);
     });
